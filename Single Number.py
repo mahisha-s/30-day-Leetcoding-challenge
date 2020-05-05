c05a67674df8e4b6f3901174026c68e4589e8bdf
mahisha-s <56316246+mahisha-s@users.noreply.github.com>
2020-05-05 18:39:46 +0530

    Create Single Number.py
    Week 1 Single Number
    Language: Python3

---
+++ Single Number.py
@@ -0,0 +1,7 @@
+#Python3
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        s=list(set(nums))
+        l=[nums.count(i) for i in s]
+        i=l.index(1)
+        return s[i]
